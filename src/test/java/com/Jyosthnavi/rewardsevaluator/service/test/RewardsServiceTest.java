606a3827527df64b0118964e39133a1a0b5c4742
jyosthnavidev <jyosthnavideveloper.com>
2022-01-18 10:02:49 -0500

    added place holder for unit test

--- src/test/java/com/Jyosthnavi/rewardsevaluator/service/test/RewardsServiceTest.java
+++ src/test/java/com/Jyosthnavi/rewardsevaluator/service/test/RewardsServiceTest.java
@@ -1,5 +1,14 @@
 package com.Jyosthnavi.rewardsevaluator.service.test;
 
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.Jyosthnavi.rewardsevaluator.service.RewardsService;
+
+@SpringBootTest
 public class RewardsServiceTest {
 
+	@InjectMocks
+	private RewardsService rewardsService;
 }
